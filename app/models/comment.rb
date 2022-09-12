b5a671cd27c44d837fd610d2d08f6daa62078055
Yeiner CQ <ycarvajalq@gmail.com>
2022-09-12 18:49:20 -0500

    Concern use for status validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,13 +11,7 @@
 #  status     :string
 #
 class Comment < ApplicationRecord
+  include Visible
+  
   belongs_to :article
-
-  VALID_STATUSES = [ 'public', 'private', 'archived' ]
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
